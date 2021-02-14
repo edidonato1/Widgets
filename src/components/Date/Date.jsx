218bf98029c808f529c0710d78c6974515660509
Eddie <edward.f.didonato@gmail.com>
2021-02-13 19:00:58 -0500

    initial commit

---
+++ src/components/Date/Date.jsx
@@ -0,0 +1,11 @@
+import { dayString, day, month, date, suffix } from './dateHelpers';
+
+export default function Date() {
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/components/Date/dateHelpers.js
@@ -0,0 +1,29 @@
+let d = new Date();
+
+export let dayString = d.toLocaleDateString().slice(0, -5).split('/').join(' / ');
+
+let days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+let months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+
+export let day = days[d.getDay()]; 
+export let month = months[d.getMonth()];
+export let date = d.getDate();
+export let suffix = '';
+  
+switch (true) {
+  case date > 3 && date < 21:
+    suffix = 'th';
+    break;
+  case date.toString().endsWith('1'):
+    suffix = 'st';
+    break;
+  case date.toString().endsWith('2'):
+    suffix = 'nd';
+    break;
+  case date.toString().endsWith('3'):
+    suffix = 'rd';
+    break;
+  default:
+    suffix = 'th';
+}
+
