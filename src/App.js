19d6fd97f62f40a7c41d2a7dece0cb7b45a08128
Eddie <edward.f.didonato@gmail.com>
2021-02-15 18:20:59 -0500

    configure numbers array for digital clock

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Clock from './widgets/Clock/Clock';
 
 function App() {
   return (
     <div >
+      <Clock />
     </div>
   );
 }
--- src/widgets/Clock/Clock.jsx
+++ src/widgets/Clock/Clock.jsx
@@ -1,5 +1,23 @@
+import { numbers } from './numbers';
+console.log(numbers[1][1])
 
+let time = [1, 2,10, 2, 3];
 
 export default function Clock() {
-  
+  return (
+    <div id="clock-main">
+      {time.map(n => 
+         <div className={n < 10 ? "analog-digit-container" : "colon-container"}>
+         {numbers[n].map(row => 
+           <div className="row">
+             {row.map(num => 
+               <div className={num === 1 ? "block on" : "block off"}>
+               </div>
+             )}
+           </div>
+         )}
+       </div>
+      )}
+    </div>
+  )
 }
